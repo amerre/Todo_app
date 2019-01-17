7f3b1fdce775aaeb910be0caec8ecbdb586a239b
amerre <adr.merre@gmail.com>
2019-01-17 09:05:52 +0100

    :lipstick: Organizing sass files

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,10 @@
   <div id="app">
     <h1>Todos.</h1>
     <div id="wrapper">
-      <!-- Input bar -->
       <app-add></app-add>
-      <!-- Nav bar -->
       <app-nav></app-nav>
-      <!-- Todo items -->
       <app-todo v-if="$store.state.show"></app-todo>
-      <!-- Done items -->
       <app-done v-if="$store.state.show"></app-done>
-      <!-- Only Todo or Done items -->
       <router-view></router-view>
     </div>
   </div>
@@ -33,23 +28,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "./assets/styles/reset.scss";
-@import "./assets/styles/background.scss";
-@import "./assets/styles/texts.scss";
-@import "./assets/styles/transitions.scss";
-
-#app {
-  flex-direction: column;
-  align-items: center;
-  padding: 50px 0px;
-  display: flex;
-  #wrapper {
-    background-color: white;
-    width: 450px;
-    padding: 20px;
-    -webkit-box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    -moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  }
-}
+@import "./assets/styles/main.scss";
 </style>
